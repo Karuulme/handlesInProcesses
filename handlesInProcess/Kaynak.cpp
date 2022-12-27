e5c32d215080c27374069ea1df592bbe20be913b
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-12-27 12:56:36 +0300

    Proje dosyası ekle.

---
+++ handlesInProcess/Kaynak.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <Psapi.h>
+#include <Shlwapi.h>
+#include <vector>
+#define NT_SUCCESS(x) ((x) >= 0)
+#pragma comment(lib, "Ntdll.lib")
+#pragma comment(lib, "Shlwapi.lib")
+constexpr unsigned int STATUS_SUCCESS = 0x0;
+constexpr unsigned int STATUS_INFO_LENGTH_MISMATCH = 0x0C0000004;
+using namespace std;
+typedef NTSTATUS(NTAPI* _NtQuerySystemInformation)(
+	ULONG  SystemInformationClass,
+	PVOID  SystemInformation,
+	ULONG  SystemInformationLength,
+	PULONG ReturnLength
+	);
+typedef NTSTATUS(NTAPI* _NtDuplicateObject)(
+	HANDLE       SourceProcessHandle,
+	HANDLE       SourceHandle,
+	HANDLE       TargetProcessHandle,
+	PHANDLE      TargetHandle,
+	ACCESS_MASK  DesiredAccess,
+	ULONG        HandleAttributes,
+	ULONG        Options
+	);
+typedef  NTSTATUS(NTAPI* _NtQueryObject)(
+	HANDLE    Handle,
+	ULONG	  ObjectInformationClass,
+	PVOID     ObjectInformation,
+	ULONG     ObjectInformationLength,
+	PULONG    ReturnLength
+	);
+typedef struct _SYSTEM_HANDLE_TABLE_ENTRY_INFO
+{
+	ULONG ProcessId;
+	BYTE ObjectTypeNumber;
+	BYTE Flags;
+	USHORT Handle;
+	PVOID Object;
+	ACCESS_MASK GrantedAccess;
+
+}SYSTEM_HANDLE_TABLE_ENTRY_INFO, * PSYSTEM_HANDLE_TABLE_ENTRY_INFO;
+typedef struct _SYSTEM_HANDLE_INFORMATION
+{
+	ULONG HandleCount;
+	SYSTEM_HANDLE_TABLE_ENTRY_INFO Handles[1];
+} SYSTEM_HANDLE_INFORMATION, * PSYSTEM_HANDLE_INFORMATION;
+typedef enum _POOL_TYPE
+{
+	NonPagedPool,
+	PagedPool,
+	NonPagedPoolMustSucceed,
+	DontUseThisType,
+	NonPagedPoolCacheAligned,
+	PagedPoolCacheAligned,
+	NonPagedPoolCacheAlignedMustS
+} POOL_TYPE, * PPOOL_TYPE;
+typedef struct _UNICODE_STRING
+{
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+typedef struct _OBJECT_TYPE_INFORMATION
+{
+	UNICODE_STRING Name;
+	ULONG TotalNumberOfObjects;
+	ULONG TotalNumberOfHandles;
+	ULONG TotalPagedPoolUsage;
+	ULONG TotalNonPagedPoolUsage;
+	ULONG TotalNamePoolUsage;
+	ULONG TotalHandleTableUsage;
+	ULONG HighWaterNumberOfObjects;
+	ULONG HighWaterNumberOfHandles;
+	ULONG HighWaterPagedPoolUsage;
+	ULONG HighWaterNonPagedPoolUsage;
+	ULONG HighWaterNamePoolUsage;
+	ULONG HighWaterHandleTableUsage;
+	ULONG InvalidAttributes;
+	GENERIC_MAPPING GenericMapping;
+	ULONG ValidAccess;
+	BOOLEAN SecurityRequired;
+	BOOLEAN MaintainHandleCount;
+	USHORT MaintainTypeList;
+	POOL_TYPE PoolType;
+	ULONG PagedPoolUsage;
+	ULONG NonPagedPoolUsage;
+} OBJECT_TYPE_INFORMATION, * POBJECT_TYPE_INFORMATION;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ handlesInProcess/handlesInProcess.cpp
@@ -0,0 +1,36 @@
+﻿#include "kaynak.cpp"
+#include <Processthreadsapi.h>
+int main() {
+	_NtQuerySystemInformation NtQuerySystemInformation = (_NtQuerySystemInformation)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQuerySystemInformation");
+	_NtDuplicateObject NtDuplicateObject = (_NtDuplicateObject)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtDuplicateObject");
+	_NtQueryObject NtQueryObject = (_NtQueryObject)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQueryObject");
+	HANDLE processHandle;
+	PSYSTEM_HANDLE_INFORMATION sProInfo=(PSYSTEM_HANDLE_INFORMATION)malloc(sizeof(sProInfo));
+	NTSTATUS ntStatus = STATUS_INFO_LENGTH_MISMATCH;
+	DWORD dwRet;
+	DWORD dwSize = 0x0; 
+	while (true)
+	{
+		// VirtualFree(sProInfo, 0x0, MEM_RELEASE);
+		sProInfo = (PSYSTEM_HANDLE_INFORMATION)VirtualAlloc(nullptr, dwSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+		ntStatus = NtQuerySystemInformation(16, sProInfo, (ULONG)dwSize, &dwRet);
+		if (ntStatus == STATUS_SUCCESS) { break; }
+		else if (ntStatus != STATUS_INFO_LENGTH_MISMATCH) {
+			VirtualFree(sProInfo, 0x0, MEM_RELEASE);
+			sProInfo = nullptr;
+			return 0x1;
+		}
+		dwSize = dwRet + (2 << 12);
+	}
+	cout<<(int)sProInfo->HandleCount;
+
+	for (int i = 0; i < sProInfo->HandleCount;i++) {
+		SYSTEM_HANDLE_TABLE_ENTRY_INFO handle = sProInfo->Handles[i];
+		if (handle.ProcessId==2412) {
+			//= OpenProcess(PROCESS_ALL_ACCESS, FALSE, 2412);
+			printf_s("Handle 0x%x at 0x%p, PID: %x\n", handle.Handle, handle.Object, handle.ProcessId);
+		}	
+	}
+	CloseHandle(processHandle);
+	return 0;
+}
