a9204f3a0b1e8e9f2b316754573f2db0007eef12
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-12-31 17:53:33 +0300

    get handles of process

--- handlesInProcess/main.cpp
+++ handlesInProcess/main.cpp
@@ -1,12 +1,33 @@
 ﻿#include "library.cpp"
 #include <Processthreadsapi.h>
-int main() {
-	_NtQuerySystemInformation NtQuerySystemInformation = (_NtQuerySystemInformation)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQuerySystemInformation");
-	_NtDuplicateObject NtDuplicateObject = (_NtDuplicateObject)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtDuplicateObject");
-	_NtQueryObject NtQueryObject = (_NtQueryObject)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQueryObject");
+#include<map>
+class processListClass {
+public:
+	map<DWORD, HANDLE> processList;
 
-	int pid=3696;
-	HANDLE processHandle=OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	void added(DWORD* key) {
+		processList[*key] = 0;
+	}
+	HANDLE* getHandle(DWORD key) {
+		return &processList.find(key)->second;
+	}
+	~processListClass() {
+		std::map<DWORD, HANDLE>::iterator it = processList.begin();
+		while (it != processList.end())
+		{
+			CloseHandle(it->second);
+			it++;
+		}
+	}
+	void handleClose(DWORD pid) {
+		CloseHandle(processList.find(pid)->second);
+		processList.erase(pid);
+	}
+};
+int getProcessIDList(processListClass* process);
+/*
+int getHandles(DWORD * pid, processListClass* processList, _NtQuerySystemInformation* NtQuerySystemInformation, _NtDuplicateObject *NtDuplicateObject, _NtQueryObject *NtQueryObject)
+{
 	PSYSTEM_HANDLE_INFORMATION handleLis = (PSYSTEM_HANDLE_INFORMATION)malloc(sizeof(PSYSTEM_HANDLE_INFORMATION));
 	NTSTATUS ntStatus = STATUS_INFO_LENGTH_MISMATCH;
 	DWORD dwRet;
@@ -16,7 +37,7 @@ int main() {
 	{
 		VirtualFree(handleLis, 0x0, MEM_RELEASE);
 		handleLis = (PSYSTEM_HANDLE_INFORMATION)VirtualAlloc(nullptr, dwSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-		ntStatus = NtQuerySystemInformation(16, handleLis, (ULONG)dwSize, &dwRet);
+		ntStatus = (*NtQuerySystemInformation)(16, handleLis, (ULONG)dwSize, &dwRet);
 		if (ntStatus == STATUS_SUCCESS) { break; }
 		else if (ntStatus != STATUS_INFO_LENGTH_MISMATCH) {
 			VirtualFree(handleLis, 0x0, MEM_RELEASE);
@@ -25,46 +46,79 @@ int main() {
 		}
 		dwSize = dwRet + (2 << 12);
 	}
-	for (int i = 0; i < handleLis->HandleCount; i++) 
+	for (int i = 0; i < handleLis->HandleCount; i++)
 	{
 		SYSTEM_HANDLE_TABLE_ENTRY_INFO handle = handleLis->Handles[i];
 		HANDLE dupHan;
-		POBJECT_TYPE_INFORMATION handlTeype=(POBJECT_TYPE_INFORMATION)malloc(0x1000);
-		PULONG	ReturnLength=0;
+		POBJECT_TYPE_INFORMATION handlTeype = (POBJECT_TYPE_INFORMATION)malloc(0x1000);
+		PULONG	ReturnLength = 0;
 		OBJECT_INFORMATION_CLASS handleReferance;
 		PVOID objectNameInfo = {};
 		UNICODE_STRING objectName;
-
-		if (handle.ProcessId == pid) {
-			if (NtDuplicateObject(processHandle, (HANDLE)handle.Handle, GetCurrentProcess(), &dupHan, 0, 0, 0) != STATUS_SUCCESS)
-				continue;
+		 
+		if (handle.ProcessId == *pid) {
+			if ((*NtDuplicateObject)(*processList->getHandle(*pid), (HANDLE)handle.Handle, GetCurrentProcess(), &dupHan, 0, 0, 0) != STATUS_SUCCESS)
+				goto next;
 			handleReferance = ObjectType;
-			if (NtQueryObject(dupHan, handleReferance, handlTeype, 0x1000, ReturnLength) < 0) {
-				if(NtQueryObject(dupHan, handleReferance, handlTeype, *ReturnLength, NULL)<0)
+			if ((*NtQueryObject)(dupHan, handleReferance, handlTeype, 0x1000, ReturnLength) < 0) {
+				if ((*NtQueryObject)(dupHan, handleReferance, handlTeype, *ReturnLength, NULL) < 0)
 					goto next;
 			}
-			handleReferance =ObjectName;
-			ReturnLength=0;
+			handleReferance = ObjectName;
+			ReturnLength = 0;
 			objectNameInfo = malloc(0x1000);
-			if (NtQueryObject(dupHan, handleReferance, objectNameInfo, 0x1000, ReturnLength) < 0) {
-				if (NtQueryObject(dupHan, handleReferance, objectNameInfo, *ReturnLength, NULL) < 0)
+			if ((*NtQueryObject)(dupHan, handleReferance, objectNameInfo, 0x1000, ReturnLength) < 0) {
+				if ((*NtQueryObject)(dupHan, handleReferance, objectNameInfo, *ReturnLength, NULL) < 0)
 					goto next;
 			}
 			objectName = *(PUNICODE_STRING)objectNameInfo;
-			
-			(objectName.Length) ? printf("%.*S: %.*S\n", handlTeype->Name.Length / 2, handlTeype->Name.Buffer, objectName.Length / 2, objectName.Buffer):NULL;
+			(objectName.Length) ? printf("%.*S: %.*S\n", handlTeype->Name.Length / 2, handlTeype->Name.Buffer, objectName.Length / 2, objectName.Buffer) : NULL;
 		next:
 			free(handlTeype);
 			free(objectNameInfo);
 			CloseHandle(dupHan);
-		}	
+		}
 	}
+	free(handleLis);
+	return 0;
+}*/
+int main() {
+	_NtQuerySystemInformation NtQuerySystemInformation = (_NtQuerySystemInformation)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQuerySystemInformation");
+	_NtDuplicateObject NtDuplicateObject = (_NtDuplicateObject)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtDuplicateObject");
+	_NtQueryObject NtQueryObject = (_NtQueryObject)GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtQueryObject");
+	//string a;
+	processListClass processList;
+	getProcessIDList(&processList);
+	std::map<DWORD, HANDLE>::iterator it = processList.processList.begin();
+	while (it != processList.processList.end())
+	{
+		//openProcessHandle(it->first, &processList,&a);	
+		HANDLE processHandle = OpenProcess(PROCESS_DUP_HANDLE, FALSE,(DWORD)it->first);
+		if (processHandle != NULL) {
+			cout << "BASARILI" << endl;
+			DuplicateHandle(GetCurrentProcess(), processHandle, GetCurrentProcess(), processList.getHandle(it->first), 0, FALSE, DUPLICATE_SAME_ACCESS);
+		}
+		it++;
+		CloseHandle(processHandle);
+	}
+}
+int getProcessIDList(processListClass* process) {
+	HANDLE hProcessShot;
+	PROCESSENTRY32 ProcessInformation;
+	hProcessShot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	ProcessInformation.dwSize = sizeof(PROCESSENTRY32);
+	if (Process32First(hProcessShot, &ProcessInformation) && INVALID_HANDLE_VALUE != hProcessShot) {
+		do
+		{
+			process->added(&ProcessInformation.th32ProcessID);
+		} while (Process32Next(hProcessShot, &ProcessInformation));
+	}
+	else {
 
-	//free(handleLis);
-	CloseHandle(processHandle);
+	}
+	CloseHandle(hProcessShot);
 	return 0;
 }
-
 /*
 http://undocumented.ntinternals.net/index.html?page=UserMode%2FUndocumented%20Functions%2FNT%20Objects%2FType%20independed%2FNtQueryObject.html
 https://learn.microsoft.com/en-us/windows/win32/api/winternl/nf-winternl-ntqueryobject
@@ -72,5 +126,4 @@ https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/ntifs/nf-ntifs-zw
 https://github.com/Zer0Mem0ry/WindowsNT-Handle-Scanner/blob/master/FindHandles/main.cpp
 https://cplusplus.com/forum/windows/95774/
 https://www.geoffchappell.com/studies/windows/km/ntoskrnl/api/ex/sysinfo/query.htm
-
 */
